3c3d402a16a9550ab515c8d3ec8ab90e313a1035
Phillip Scramlin <phillip.scramlin78@gmail.com>
2017-01-30 02:14:38 -0500

    Added timeout to achieve goal

--- catkin_ws/src/adventurer/nodes/explorer.py
+++ catkin_ws/src/adventurer/nodes/explorer.py
@@ -6,35 +6,54 @@ import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 #from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 
-seq_id = 0
+max_trial_time = 600
+max_wait_time = 35
 
+# Tested with jackal navigation tutorial
 
 def getGoal():
 
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = 'map'
-    goal.target_pose.pose.position.x = -10.1
-    goal.target_pose.pose.position.y = 15.1
+    goal.target_pose.pose.position.x = 1
+    goal.target_pose.pose.position.y = 1
     goal.target_pose.pose.position.z = 0
     goal.target_pose.pose.orientation.x = 0
     goal.target_pose.pose.orientation.y = 0
     goal.target_pose.pose.orientation.z = 1
+    rospy.loginfo("New Goal Generated")
 
-    client.send_goal(goal)
-    client.wait_for_result()
+    return goal
 
 
 if __name__ == "__main__":
 
+    # arguements for map length and width
+   # x = sys.argv[1]
+   # y = sys.argv[2]
+
     rospy.init_node('explorer', anonymous=True)
     rate = rospy.Rate(10)
     client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+    done = False
 
     try:
+        # Get Randomized Goal Pose values
+        goal = getGoal()
 
-        client.wait_for_server()
-        getGoal()
-        rate.sleep()
+        client.send_goal(goal)
+        #client.wait_for_result()
+        goal_time = time.time()
+        # Terminate Goal after max_wait_time
+        while not done:
+
+            current_time = time.time()
+            if((goal_time + max_wait_time) < current_time):
+                done = True
+                rospy.loginfo("Setting done to true")
+            rate.sleep()
+        done = False
 
     except rospy.ROSInterruptException:
         pass
