08bfafcdbbe9a448b2c7029f06b49e47bba88db0
mano <spalania@mtu.edu>
2017-01-31 13:32:51 -0600

    Stop goal action after ten minutes

--- catkin_ws/src/adventurer/nodes/explorer.py
+++ catkin_ws/src/adventurer/nodes/explorer.py
@@ -4,17 +4,23 @@ import rospy
 import time
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+import random
 
 # from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
 
+# set the maximum time allowed for mapping
 max_trial_time = 600
-max_wait_time = 35
-
-# Tested with jackal navigation tutorial
 
 
-def get_goal():
+# function to get a random goal pose
+# def get_goal():
+    # Get Randomized Goal Pose value
+    # goal = set_goal()
+#    pass
 
+
+# function to set the random goal
+def set_goal():
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = 'map'
     goal.target_pose.pose.position.x = 10
@@ -41,21 +47,22 @@ if __name__ == "__main__":
     done = False
 
     try:
-        # Get Randomized Goal Pose values
-        goal = get_goal()
-
+        goal = set_goal()
         client.send_goal(goal)
         # client.wait_for_result()
-        goal_time = time.time()
+        init_time = time.time()
         # Terminate Goal after max_wait_time
         while not done:
-
             current_time = time.time()
-            if (goal_time + max_wait_time) < current_time:
+            # enters if loop once max_wait_time is reached
+            if (current_time - init_time) > max_trial_time:
                 done = True
+                # cancel all predefined goals set for the robot
+                client.cancel_all_goals()
                 rospy.loginfo("Setting done to true")
             rate.sleep()
         done = False
 
     except rospy.ROSInterruptException:
         pass
+
